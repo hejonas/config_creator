28081d50a5ceca1be1a7be1a2d36053657ef3b22
Jonas Hellberg <jonas.hellberg@diadrom.se>
2022-07-31 11:41:33 +0200

    Added logging functionality

--- config_creator.py
+++ config_creator.py
@@ -3,10 +3,10 @@
 import sys
 import os
 import argparse
-import string
+import logging
 
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser(
         description='Generates a config file template for a C repository'
     )
@@ -14,13 +14,26 @@ if __name__ == '__main__':
         metavar='path',
         type=str, 
         help='path of the top level folder') 
+
+    parser.add_argument('--loglevel',
+        type=str,
+        default='WARNING',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        help='Set level of logging. Default is warning.')
     
     args = parser.parse_args()
 
     input_path = args.Path
-    
+    loglevel = args.loglevel
+
+    # Configure logger
+    # TODO: consider having an option for inputting the log file name
+    logging.basicConfig(filename='log.log',
+                        filemode='w',
+                        level=loglevel) 
+
     if not os.path.isdir(input_path):
-        print('The path specified does not exist')
+        logging.error('The path specified does not exist')
         sys.exit(1)
 
     print('\n'.join(os.listdir(input_path)))
